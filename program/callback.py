e6c32f3229b3ffc0cbc227dcf96a2707e7cfccd5
Duckoi1707 <91252600+Duckoi1707@users.noreply.github.com>
2021-11-03 20:28:16 +0700

    Update callback.py

--- program/callback.py
+++ program/callback.py
@@ -102,7 +102,7 @@ async def cbbasic(_, query: CallbackQuery):
 
 » /play (song name/link) - play music on video chat
 » /stream (query/link) - stream the yt live/radio live music
-» /vplay (video name/link) - play video on video chat
+» /batv (Tên Video Hoặc Đường Giẫn Video) - play video on video chat
 » /vstream - play live video from yt live/m3u8
 » /playlist - show you the playlist
 » /video (query) - download video from youtube
@@ -111,8 +111,8 @@ async def cbbasic(_, query: CallbackQuery):
 » /search (query) - search a youtube video link
 
 » /ping - show the bot ping status
-» /uptime - show the bot uptime status
-» /alive - show the bot alive info (in group)
+» /thoigian - show the bot uptime status
+» /thongtin - show the bot alive info (in group)
 
 ⚡️ __Powered by {BOT_NAME} AI__""",
         reply_markup=InlineKeyboardMarkup(
